47c3b69cf2a7c9026a7fb833d8298ba206db3149
kosi <kossyfab@gmail.com>
2024-12-17 15:36:34 +1100

    Model training pipeline created

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from src.textSummarizer.logging import logger
 from src.textSummarizer.pipeline.data_ingestion_pipeline import DataIngestionTrainingPipeline
 from src.textSummarizer.pipeline.data_transformation_pipeline import DataTransformationTrainingPipeline
+from src.textSummarizer.pipeline.model_trainer_pipeline import ModelTrainerTrainingPipeline
 
 STAGE_NAME = "Data Ingestion Stage"
 
@@ -18,6 +19,17 @@ try:
     logger.info(f"{STAGE_NAME} initiated")
     data_transformation_pipeline = DataTransformationTrainingPipeline()
     data_transformation_pipeline.initiate_data_transformation()
+    logger.info(f"{STAGE_NAME} completed")
+except Exception as e:
+    logger.exception(e)
+
+
+STAGE_NAME = "Model Training Stage"
+
+try:
+    logger.info(f"{STAGE_NAME} initiated")
+    model_trainer_pipeline = ModelTrainerTrainingPipeline()
+    model_trainer_pipeline.initiate_model_training()
     logger.info(f"{STAGE_NAME} completed")
 except Exception as e:
     logger.exception(e)
